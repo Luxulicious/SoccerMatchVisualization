3073873e7f693d62e98a418575e83ea6124f8642
Lux <9366973+Luxulicious@users.noreply.github.com>
2021-07-07 15:02:00 +0200

    Added a ReplayPlayer
    Needs a lot more functionality, but loading is functional

---
+++ Assets/ReplayPlayer.cs
@@ -0,0 +1,98 @@
+﻿using Assets.Scripts;
+using Sirenix.OdinInspector;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TheLuxGames.Visualizer.Domain;
+using UnityEditor;
+using UnityEngine;
+
+public class ReplayPlayer<TReplay, TBall, TPlayer> : MonoBehaviour
+        where TReplay : Replay, new()
+        where TBall : Ball, new()
+        where TPlayer : Player, new()
+{
+    [SerializeReference, HideInInspector] private IReader _readerAsReference;
+    [SerializeField, HideInInspector] private UnityEngine.Object _readerAsField;
+
+    [ShowInInspector]
+    public IReader Reader 
+    { 
+        get 
+        {
+            if (_readerAsReference != null) return _readerAsReference;
+            else if (_readerAsField != null) return _readerAsReference as IFileReader<TReplay>;
+            else return null;
+        } 
+        set 
+        {
+            if (value is UnityEngine.Object O)
+            {
+                _readerAsField = O;
+                _readerAsReference = null;
+            }
+            else
+            {
+                _readerAsReference = value;
+                _readerAsField = null;
+            }
+        } 
+    }
+
+    [SerializeField] private TReplay _replay;
+    [SerializeField] private int? currentFrame = null;
+    
+    [ShowInInspector]
+    public int CurrentFrame
+    {
+        get 
+        {
+            if (currentFrame == null)
+            {
+                var frame = _replay.FirstFrameIndex;
+                return frame != null ? frame.Value : -1;
+            }
+            return currentFrame.Value; 
+        }
+        set
+        {
+            if (!_replay.Frames.Any(x => x.FrameIndex == value))
+            { 
+                Debug.LogError($"No frame found at index: {value}");
+                return;
+            }
+            currentFrame = value;
+        }
+    }
+
+    [Button("Load Replay")]
+    protected virtual void Load()
+    {
+        if (Reader == null)
+        {
+            Debug.LogError("Reader cannot be left empty!");
+            return;
+        }
+        var reader = Reader as IFileReader<TReplay>;
+        if (reader == null)
+        {
+            Debug.LogError("Invalid reader");
+            return;
+        }
+        var filePath = EditorUtility.OpenFilePanel("Select a file to extract replay data from", "Assets/", "dat");
+        if (string.IsNullOrEmpty(filePath))
+        {
+            Debug.LogError("Filepath is empty");
+            return;
+        }
+        if (!File.Exists(filePath))
+        {
+            Debug.LogError($"Failed to find file at path: '{filePath}'");
+            return;
+        }
+
+        _replay = reader.ReadFromFile(filePath);
+    }
+}
---
+++ Assets/Scripts/SoccerReplayPlayer.cs
@@ -0,0 +1,13 @@
+﻿using Assets.Scripts.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    public class SoccerReplayPlayer : ReplayPlayer<SoccerReplay, SoccerBall, SoccerPlayer>
+    {
+    }
+}
