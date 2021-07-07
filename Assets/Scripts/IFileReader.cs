ffd97a6edf05108b16046ae8d9df34ab40a6ba36
Lux <9366973+Luxulicious@users.noreply.github.com>
2021-07-07 14:55:45 +0200

    Added interface for reading files

---
+++ Assets/Scripts/IFileReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Assets.Scripts
+{
+    public interface IReader
+    { 
+        
+    }
+
+    public interface IFileReader<TOutput> : IReader
+    {
+        TOutput ReadFromFile(string path);
+    }
+
+}
