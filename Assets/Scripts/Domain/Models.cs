ab1ec1cf9e5f346901ce3b4b36bf2233806e4ab5
Lux <9366973+Luxulicious@users.noreply.github.com>
2021-07-08 13:21:34 +0200

    Decided to make id non-nullable to support native Unity serialization

--- Assets/Scripts/Domain/Models.cs
+++ Assets/Scripts/Domain/Models.cs
@@ -74,12 +74,12 @@ namespace TheLuxGames.Visualizer.Domain
     [Serializable]
     public abstract class Object
     {
-        [SerializeField] private int? _id;
+        [SerializeField] private int _id = -1;
         //TODO Maybe not depend on Unity vector3
         [SerializeField] private Vector3 _position;
 
         public Vector3 Position { get => _position; set => _position = value; }
-        public int? Id { get => _id; set => _id = value; }
+        public int Id { get => _id; set => _id = value; }
     }
 
     [Serializable]
