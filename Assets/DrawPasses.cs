f0681ca2e9fc743b8ab18e7fbc5fb31aa931ff5a
Lux <9366973+Luxulicious@users.noreply.github.com>
2021-07-20 20:22:17 +0200

    Add draw passes materials and colors

--- Assets/DrawPasses.cs
+++ Assets/DrawPasses.cs
@@ -9,6 +9,14 @@ using UnityEngine;
 
 public class DrawPasses : MonoBehaviour
 {
+
+    public struct Threat 
+    {
+        public float distanceFromReceiver;
+        public float distanceFromPasser;
+        public bool inPassline;
+    }
+
     [SerializeField, Required] private int _teamId;
     [SerializeField, Required] private int[] _validTeamIds;
     [SerializeField, Required] private Transform _playerTransform;
@@ -17,6 +25,7 @@ public class DrawPasses : MonoBehaviour
     [SerializeField, Required] private GameObject _lineRendererPrefab;
     [SerializeField, Required] private Transform _lineRendererParentTransform;
     [SerializeField, ReadOnly] private List<SoccerPlayerComponent> _allSoccerPlayers = new List<SoccerPlayerComponent>();
+    [SerializeField, Required] private SoccerPlayerComponent _passer;
     [SerializeField] private float _heightOffsetLines = 0.1125f;
     [SerializeField] private bool _drawing = false;
     
@@ -47,7 +56,7 @@ public class DrawPasses : MonoBehaviour
         }
     }
 
-    public List<SoccerPlayerComponent> AllOpposingTeamPlayers
+    public List<SoccerPlayerComponent> AllEnemies
     {
         get
         {
@@ -90,7 +99,15 @@ public class DrawPasses : MonoBehaviour
         while (_lineRenderers.Count < AllFellowTeamPlayers.Count) AddLineRenderer();
         for (int i = 0; i < AllFellowTeamPlayers.Count; i++)
         {
-            _lineRenderers[i].SetPositions(new Vector3[] { _playerTransform.position + (Vector3.up * _heightOffsetLines), AllFellowTeamPlayers[i].transform.position + (Vector3.up * _heightOffsetLines) });
+            SoccerPlayerComponent from = _passer;
+            SoccerPlayerComponent to = AllFellowTeamPlayers[i];
+            var safety = GetPassSafetyPercentage(from, to);
+            var safetyEvaluation = safety / 100;
+            Color color = _gradient.Evaluate(safetyEvaluation);
+            _lineRenderers[i].startColor = color;
+            _lineRenderers[i].endColor = color;
+            _lineRenderers[i].SetPositions(new Vector3[] { _playerTransform.position + (Vector3.up * _heightOffsetLines), to.transform.position + (Vector3.up * _heightOffsetLines) });
+
         }
     }
 
@@ -100,4 +117,92 @@ public class DrawPasses : MonoBehaviour
         var lineRenderer = lineRendererGO.GetComponent<LineRenderer>();
         _lineRenderers.Add(lineRenderer);
     }
+
+    public float GetPassSafetyPercentage(SoccerPlayerComponent passer, SoccerPlayerComponent receiver)
+    {
+        Vector3 fromPosition = passer.Value.Position;
+        Vector3 receiverPos = receiver.Value.Position;
+
+        var threats = GetPassThreats(fromPosition, receiverPos);
+        float safety = CalculateSafety(threats);
+        return safety;
+    }
+
+    private static float CalculateSafety(List<Threat> threats)
+    {
+        float baseSafety = 100f;
+        float safety = baseSafety;
+        //TODO This calculation is very primitives and needs improvement obv 
+        if (threats.Any()) safety -= baseSafety / 2 * threats.Count;
+        if (safety < 0) safety = 0;
+        return safety;
+    }
+
+    private List<Threat> GetPassThreats(Vector3 fromPosition, Vector3 receiverPos)
+    {
+        var threats = new List<Threat>();
+        var passLineWidth = 1f;
+        var threatRadius = 5f;
+
+        var passLineCoordinates = GetPassLineAreaCoordinates(fromPosition, receiverPos, passLineWidth);
+        AllEnemies.ForEach(enemy =>
+        {
+            Vector2 enemyPosition = enemy.Value.Position;
+            var enemyInPassLine = PointInsidePolygon(passLineCoordinates, enemyPosition);
+            var enemyInReceiverThreatRadius = PointInsideCircle(threatRadius, receiverPos, enemyPosition);
+            var enemyInThreatRange = enemyInPassLine || enemyInReceiverThreatRadius;
+
+            if (enemyInThreatRange)
+            {
+                Threat threat = new Threat();
+                float distanceFromReceiver = Vector2.Distance(enemyPosition, receiverPos);
+                var inThreatRadiusOfReceiver = distanceFromReceiver <= threatRadius;
+                if (inThreatRadiusOfReceiver) threat.distanceFromReceiver = distanceFromReceiver;
+
+                float distanceFromPasser = Vector2.Distance(enemyPosition, fromPosition);
+                var inThreatRadiusOfPasser = distanceFromPasser <= threatRadius;
+                if (inThreatRadiusOfPasser) threat.distanceFromPasser = distanceFromPasser;
+
+                threat.inPassline = enemyInPassLine;
+
+                threats.Add(threat);
+            }
+        });
+        return threats;
+    }
+    
+    private Vector2[] GetPassLineAreaCoordinates(Vector2 from, Vector2 to, float width)
+    {
+        var axis = (to - from).normalized;
+        return GetAxisAlignedRectangleCoordinates(from, to, width, axis);
+    }
+
+    //TODO Move these math functions below to their own class
+    private static Vector2[] GetAxisAlignedRectangleCoordinates(Vector2 start, Vector2 end, float width, Vector2 axis)
+    {
+        Vector2 perpendicularDir = new Vector2(-axis.y, axis.x);
+        float extents = width / 2;
+        Vector2 a = start - extents * perpendicularDir;
+        Vector2 b = start + extents * perpendicularDir;
+        Vector2 c = end + extents * perpendicularDir;
+        Vector2 d = end - extents * perpendicularDir;
+        return new Vector2[] { a, b, c, d };
+    }
+
+    private static bool PointInsidePolygon(Vector2[] polygon, Vector2 point)
+    {
+        //Source: http://wiki.unity3d.com/index.php?title=PolyContainsPoint&oldid=20475
+        var j = polygon.Length - 1;
+        var inside = false;
+        for (int i = 0; i < polygon.Length; j = i++)
+        {
+            if (((polygon[i].y <= point.y && point.y < polygon[j].y) || (polygon[j].y <= point.y && point.y < polygon[i].y)) &&
+               (point.x < (polygon[j].x - polygon[i].x) * (point.y - polygon[i].y) / (polygon[j].y - polygon[i].y) + polygon[i].x))
+                inside = !inside;
+        }
+        return inside;
+    }
+
+    private bool PointInsideCircle(float radius, Vector2 origin, Vector2 point) => Vector2.Distance(origin, point) <= radius;
+
 }
