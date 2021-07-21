35ecdc987a09ed7cd234e3da90e95d88e4aaa60c
Lux <9366973+Luxulicious@users.noreply.github.com>
2021-07-21 14:41:13 +0200

    Cleaning up replay

--- Assets/Scripts/Domain/Replay.cs
+++ Assets/Scripts/Domain/Replay.cs
@@ -1,4 +1,5 @@
-﻿using Sirenix.OdinInspector;
+﻿using JetBrains.Annotations;
+using Sirenix.OdinInspector;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,6 @@ namespace TheLuxGames.Visualizer.Domain
         [Range(1, 360)]
         [SerializeField] private int _frameRate = 25;
 
-        //TODO Change list to something else since it's very likely ineffecient
         [SerializeField] private Dictionary<int, Frame> _frames;
 
         public Dictionary<int, Frame> Frames
@@ -30,20 +30,10 @@ namespace TheLuxGames.Visualizer.Domain
             }
         }
 
-        [ShowInInspector]
-        private List<Frame> FramesAsList => _frames?.Values.ToList();
+        [ShowInInspector, UsedImplicitly] private List<Frame> FramesAsList => _frames?.Values.ToList();
 
         public int FrameRate { get => _frameRate; set => _frameRate = value; }
 
-        public int? FirstFrameIndex
-        {
-            get
-            {
-                if (Frames.Any())
-                    //return Frames.OrderBy(x => x.FrameIndex).FirstOrDefault().FrameIndex;
-                    return Frames.OrderBy(x => x.Key).First().Key;
-                else return null;
-            }
-        }
+        public int? FirstFrameIndex => Frames.Any() ? Frames.OrderBy(x => x.Key).First().Key : (int?)null;
     }
 }
